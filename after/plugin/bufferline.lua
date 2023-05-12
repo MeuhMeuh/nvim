cf612a6613053f463e242905236ea6c449192505
MeuhMeuh <alexis.menard@swile.co>
2023-05-12 11:47:48 +0200

    fix: few tweaks

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,7 +1,19 @@
-require("bufferline").setup{
-  options = {
-    buffer_close_icon = "⤫",
-    numbers = "buffer_id",
-  }
-}
+local bufferline = require('bufferline')
 
+bufferline.setup {
+  options = {
+    -- buffer_close_icon = "⤫",
+    show_close_icon = false,
+    numbers = "buffer_id",
+    -- TODO Doesn't work
+    highlights = {
+      fill = {
+        fg = '#FF0000',
+        bg = '#00FF00'
+      },
+      background = {
+        fg = 'red'
+      }
+    }
+  },
+}
